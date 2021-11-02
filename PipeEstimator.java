130e37e71f7d5172af4605145fdbbf9e1b6cc6af
rygall <91382298+rygall@users.noreply.github.com>
2021-11-01 22:47:00 -0400

    Add files via upload

--- PipeEstimator.java
+++ PipeEstimator.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/* @ Pipe_Estimator
+   @ Created by Ryan Gallagher */
+
 
 public class PipeEstimator {
 
@@ -15,12 +18,12 @@ public class PipeEstimator {
         int elbows90 = 0;
         int elbows45 = 0;
 
-        //initializing the two strings for units of measurement and then obtaining the input and output
-        //units of measurement, there is also while loop to make sure they enter correct units
+        /* initializing the two strings for units of measurement and then obtaining the input and output
+           units of measurement, there is also while loop to make sure they enter correct units */
         String unitsInput = "";
         String unitsOutput = "";
         while (!(unitsInput.equalsIgnoreCase("meters") || unitsInput.equalsIgnoreCase("feet") || unitsInput.equalsIgnoreCase("inches"))) {
-            System.out.println("Enter the units of measurement that you are using to measure the length of the section of pipe (meters, feet or inches): ");
+            System.out.printf("\n\n\nEnter the units of measurement that you are using to measure the length of the section of pipe (meters, feet or inches): \n");
             unitsInput = keyboard.nextLine();
             if (!(unitsInput.equalsIgnoreCase("meters") || unitsInput.equalsIgnoreCase("feet") || unitsInput.equalsIgnoreCase("inches"))) {
                 System.out.printf("\n\nERROR! INVALID UNITS OF MEASUREMENT, PLEASE ENTER UNITS OF MEASUREMENT AGAIN\n");
@@ -45,7 +48,7 @@ public class PipeEstimator {
             pipeSectionLengths.add(length);
         }
 
-        //tallying up all the lengths of the different pipe sections
+        //summing up all the lengths of the different pipe sections using an enhanced for loop
         double totalLength = 0;
         for (Double l : pipeSectionLengths) {
             totalLength += l;
@@ -54,6 +57,7 @@ public class PipeEstimator {
         //adding 25% to the total length to give the estimated length
         double estTotalLength = totalLength * 1.25;
 
+        //if statements to compute the number of 90 degree elbows
         if (unitsInput.equalsIgnoreCase("meters")) {
             elbows90 = (int) Math.ceil(estTotalLength / 2.438);
             elbows45 = (int) Math.ceil(estTotalLength / 1.22);
@@ -77,7 +81,6 @@ public class PipeEstimator {
         }
 
         //below are just more if statements to convert and print to the proper output units from the given input units
-        //
         if ((unitsInput.equalsIgnoreCase("meters")) && (unitsOutput.equalsIgnoreCase("feet"))) {
             units.metersToFeet(estTotalLength);
             System.out.printf("\n\nThe estimated total pipe length is: %.3f %s", units.getFeet(), unitsOutput);
@@ -105,13 +108,11 @@ public class PipeEstimator {
 
         if ((unitsInput.equalsIgnoreCase("inches")) && (unitsOutput.equalsIgnoreCase("feet"))) {
             units.inchesToFeet(estTotalLength);
-            System.out.printf("\n\nThe estimated total pipe length is: %.3f %s", units.getFeet(), unitsOutput);
+            System.out.printf("\n\nEstimated total pipe length is: %.3f %s", units.getFeet(), unitsOutput);
         }
 
-
-        System.out.printf("\nThe estimated number of 90 degree elbows: %d", elbows90);
-        System.out.printf("\nThe estimated number of 45 degree elbows: %d\n\n\n", elbows45);
-
+        System.out.printf("\nEstimated number of 90 degree elbows: %d", elbows90);
+        System.out.printf("\nEstimated number of 45 degree elbows: %d\n\n\n", elbows45);
 
     }
 }
